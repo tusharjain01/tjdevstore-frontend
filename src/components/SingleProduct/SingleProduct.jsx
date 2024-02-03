e2cdcbd84e0b8188813bf7e46494169c9f7abb39
Tushar Jain <75475892+tusharjain01@users.noreply.github.com>
2024-02-03 17:59:00 +0530

    built Single Product Page

--- src/components/SingleProduct/SingleProduct.jsx
+++ src/components/SingleProduct/SingleProduct.jsx
@@ -1,6 +1,63 @@
 import "./SingleProduct.scss";
+import RelatedProducts from "./RelatedProducts/RelatedProducts";
+
+import {
+    FaCartPlus, 
+    FaFacebook, 
+    FaInstagram, 
+    FaLinkedin, 
+    FaPinterest, 
+    FaTwitter
+} from 'react-icons/fa'
+
+import prod1 from "../../assets/products/watch-prod-1.webp"
+
 const SingleProduct = () => {
-    return <div>Single Product</div>;
+    return (
+        <div className="single-product-main-content">
+            <div className="layout">
+                <div className="single-product-page">
+                    <div className="left">
+                        <img src={prod1} alt="headphone" />
+                    </div>
+                    <div className="right">
+                        <span className="name">Headphone</span>
+                        <span className="price">₹ 450</span>
+                        <span className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus pariatur inventore tempora tenetur error, iusto rem exercitationem explicabo quibusdam facilis.</span>
+                        <div className="cart-buttons">
+                            <div className="quantity-buttons">
+                                <span>-</span>
+                                <span>1</span>
+                                <span>+</span>
+                            </div>
+                            <button className="add-to-cart-button">
+                                <FaCartPlus size={20} />
+                                ADD TO CART
+                            </button>
+                        </div>
+                        <span className="divider" />
+                        <div className="info-item">
+                            <span className="text-bold">
+                                Category:
+                                <span>Headphones</span>
+                            </span>
+                            <span className="text-bold">
+                                Share:
+                                <span className="social-icons">
+                                    <FaFacebook size={16} />
+                                    <FaInstagram size={16} />
+                                    <FaTwitter size={16} />
+                                    <FaPinterest size={16} />
+                                    <FaLinkedin size={16} />
+                                </span>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <RelatedProducts />
+            </div>
+        </div>
+    );
 };
 
 export default SingleProduct;
