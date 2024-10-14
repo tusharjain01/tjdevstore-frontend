52a08d9c74a767db7d9dec90d9b6e09d2543ea4b
Tushar Jain <tusharjain2016@outlook.com>
2024-10-14 15:19:11 +0530

    Converted codebase from CRA to vite project

--- src/util/api.jsx
+++ src/util/api.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const params = {
     headers : {
-        Authorization : "bearer " + process.env.REACT_APP_STRAPI_KEY 
+        Authorization : "bearer " + import.meta.VITE_STRAPI_KEY 
     },
 }
 
@@ -10,7 +10,7 @@ export const fetchDataFromApi = async(url) => {
     // console.log(process.env.REACT_APP_DEV_URL + url);
     try{
         const { data } = await axios.get(
-            process.env.REACT_APP_DEV_URL + url,
+            import.meta.VITE_APP_DEV_URL + url,
             params
         );
         return data;
